04640d8ab2bb8b59a34ddbb41aa2f791a75938da
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-27 19:01:40 -0300

    4. Quadrado

---
+++ Heranca/quadrado/Quadrado.java
@@ -0,0 +1,24 @@
+public class Quadrado extends Retangulo {
+    private double lado;
+
+    // Getter para lado
+    public double getLado() {
+        return lado;
+    }
+
+    // Setter para lado
+    public void setLado(double lado) {
+        if (lado < 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+        this.lado = lado;
+        this.largura = lado;
+        this.altura = lado;
+    }
+
+    // Sobrescrita do método toString
+    @Override
+    public String toString() {
+        return String.format("[Quadrado] %.2f", lado);
+    }
+}
