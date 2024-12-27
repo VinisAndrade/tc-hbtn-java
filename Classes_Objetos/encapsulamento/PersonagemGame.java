77254bb6953cfdbf24d0d08fc5f12f24f6aae60c
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-27 16:37:35 -0300

    Pasta encapsulamento

---
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -0,0 +1,27 @@
+public class PersonagemGame {
+    //public int saudeAtual; // Atributo público de saúde
+    //public String nome;    // Atributo público de nome
+    // Atributos privados
+    private int saudeAtual;
+    private String nome;
+
+    // Getter para saudeAtual
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    // Setter para saudeAtual
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    // Getter para nome
+    public String getNome() {
+        return nome;
+    }
+
+    // Setter para nome
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
