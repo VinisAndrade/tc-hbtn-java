75875b2f2a35539271ea8a3afacfdc27f5e10ae8
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-28 15:18:45 -0300

    0. Divisão inteira

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,13 @@
+public class Numero {
+    public static void dividir(int a, int b) {
+        try {
+            // Tentativa de realizar a divisão
+            double resultado = (double) a / b; // Cast para double para permitir divisões exatas
+        } catch (ArithmeticException e) {
+            // Captura de erro no caso de divisão por zero
+            System.out.println("Erro: Divisão por zero.");
+        } finally {
+            // O bloco finally sempre será executado, independente de erro
+            System.out.printf("Resultado da divisão: %.2f\n", (double) a / b);
+        }
+    }
