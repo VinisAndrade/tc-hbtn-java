f4fbae510a584357326a228ef8a561dabe8bc73a
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-28 15:24:37 -0300

    Update Numero.java

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,14 +1,22 @@
 public class Numero {
     public static void dividir(int a, int b) {
         try {
-            // Tentativa de realizar a divisão
-            double resultado = (double) a / b; // Cast para double para permitir divisões exatas
+            // Realiza a divisão
+            int resultado = a / b;
+            System.out.printf("%d / %d = %d\n", a, b, resultado);
         } catch (ArithmeticException e) {
-            // Captura de erro no caso de divisão por zero
-            System.out.println("Erro: Divisão por zero.");
+            // Captura a exceção de divisão por zero
+            System.out.println("Nao eh possivel dividir por zero");
         } finally {
-            // O bloco finally sempre será executado, independente de erro
-            System.out.printf("Resultado da divisão: %.2f\n", (double) a / b);
+            // Sempre será executado, imprime a linha de resultado ou erro
+            if (b != 0) {
+                // Caso não tenha erro de divisão por zero, imprime o resultado
+                int resultado = a / b;
+                System.out.printf("%d / %d = %d\n", a, b, resultado);
+            } else {
+                // Caso b seja zero, imprima a mensagem de erro
+                System.out.println("Nao eh possivel dividir por zero");
+            }
         }
     }
 }
