4c4b339cd0a67a7a36a9f43ec5afdc2276f78cb2
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-29 15:46:17 -0300

    Update FormaGeometrica.java

--- Heranca/forma/FormaGeometrica.java
+++ Heranca/forma/FormaGeometrica.java
@@ -1,5 +1,4 @@
-public abstract class FormaGeometrica {
-    // Método público para calcular a área
+public class FormaGeometrica {
     public double area() {
         throw new UnsupportedOperationException("Funcao area nao implementada");
     }
