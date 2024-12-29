603b4770206013b15eb43b39997c1d549920e23d
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-29 15:20:13 -0300

    a

--- Classes_Objetos/status/PersonagemGame.java
+++ Classes_Objetos/status/PersonagemGame.java
@@ -1,59 +1,44 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 public class PersonagemGame {
     private int saudeAtual;
     private String nome;
     private String status;
 
-    public PersonagemGame(int saudeInicial, String nomeInicial) {
-        this.setSaudeAtual(saudeInicial);
-        this.setNome(nomeInicial);
-    }
-
     public int getSaudeAtual() {
-        return this.saudeAtual;
+        return saudeAtual;
     }
 
     public void setSaudeAtual(int saudeAtual) {
-        if (saudeAtual < 0) {
-            this.saudeAtual = 0;
-        } else if (saudeAtual > 100) {
-            this.saudeAtual = 100;
-        } else {
-            this.saudeAtual = saudeAtual;
-        }
+        this.saudeAtual = saudeAtual;
 
-        if (this.saudeAtual > 0) {
-            this.status = "vivo";
-        } else {
-            this.status = "morto";
-        }
-
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
-        if (nome != null && !nome.trim().isEmpty()) {
-            this.nome = nome;
-        }
-
+        if (this.saudeAtual <= 0)
+            status = "morto";
+        else
+            status = "vivo";
     }
 
     public String getStatus() {
-        return this.status;
+        return status;
     }
 
-    public void tomarDano(int quantidadeDeDano) {
-        this.setSaudeAtual(this.saudeAtual - quantidadeDeDano);
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public void receberCura(int quantidadeDeCura) {
-        this.setSaudeAtual(this.saudeAtual + quantidadeDeCura);
+        setSaudeAtual(saudeAtual + quantidadeDeCura);
+
+        if (saudeAtual > 100)
+            saudeAtual = 100;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        setSaudeAtual(saudeAtual - quantidadeDeDano);
+
+        if (saudeAtual < 0)
+            saudeAtual = 0;
     }
 }
