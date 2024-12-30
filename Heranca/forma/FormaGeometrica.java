32204661a44de338936872ce92eee54b4d4acd8a
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-30 13:06:25 -0300

    Correções
    Sintaxe do texto e this.

--- Heranca/forma/FormaGeometrica.java
+++ Heranca/forma/FormaGeometrica.java
@@ -1,3 +1,13 @@
+public abstract class FormaGeometrica {
+    // Método público para calcular a área
+    public double area() {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
+
+
+/*Gabarito*/
+
 public class FormaGeometrica {
     public double area() {
         throw new UnsupportedOperationException("Funcao area nao implementada");
