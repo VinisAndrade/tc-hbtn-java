5e7a2d3a0a176a095e486af6b3c1c16c3e7618eb
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2024-12-30 14:35:49 -0300

    Update FileWritingExercise.java

--- arquivos/0/FileWritingExercise.java
+++ arquivos/0/FileWritingExercise.java
@@ -3,21 +3,34 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 public class FileWritingExercise {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
 
         // Solicita o nome do arquivo
         System.out.print("Digite o nome do arquivo (com extensão .txt): ");
         String fileName = scanner.nextLine();
 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            System.out.println("Digite linhas de texto (digite 'sair' para finalizar):");
 
-        // implemente o codigo aqui
+            while (true) {
+                String input = scanner.nextLine();
 
+                // Verifica se o usuário deseja sair
+                if (input.equalsIgnoreCase("sair")) {
+                    break;
+                }
 
+                // Escreve a linha no arquivo
+                writer.write(input);
+                writer.newLine();
+            }
 
+            System.out.println("Arquivo '" + fileName + "' foi criado e salvo com sucesso.");
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao criar ou salvar o arquivo: " + e.getMessage());
+        }
 
         scanner.close();
     }
