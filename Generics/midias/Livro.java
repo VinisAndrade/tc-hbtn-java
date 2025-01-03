2b038d7ccd9696e37486a6c99d3e28dc7033aee5
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-03 15:56:33 -0300

    a

--- Generics/midias/Livro.java
+++ Generics/midias/Livro.java
@@ -1,4 +1,4 @@
-/*class Livro extends Midia {
+class Livro extends Midia {
     private int edicao;
 
     public Livro(String nome, int edicao) {
@@ -8,11 +8,5 @@
 
     public int getEdicao() {
         return edicao;
-    }
-}*/
-
-class Livro extends Midia {
-    public Livro(String nome) {
-        super(nome, "Livro");
     }
 }
