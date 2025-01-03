cb877e89900706b718748754a79971e6f7c1f69f
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-03 15:47:37 -0300

    Java - Generics

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,11 @@
+class Biblioteca<T extends Midia> {
+    private List<T> midias = new ArrayList<>();
+
+    public void adicionarMidia(T midia) {
+        midias.add(midia);
+    }
+
+    public List<T> obterListaMidias() {
+        return midias;
+    }
+}
---
+++ Generics/midias/Livro.java
@@ -0,0 +1,12 @@
+class Livro extends Midia {
+    private int edicao;
+
+    public Livro(String nome, int edicao) {
+        super(nome);
+        this.edicao = edicao;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+}
