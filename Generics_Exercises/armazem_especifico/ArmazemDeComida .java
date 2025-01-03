0772989d656ebe70dc3c041d11bb1ecf87493be0
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-03 17:44:16 -0300

    a

--- Generics_Exercises/armazem_especifico/ArmazemDeComida .java
+++
@@ -1,4 +0,0 @@
-public class ArmazemDeComida extends Armazem<Comida> {
-    // Não há necessidade de implementar métodos adicionais,
-    // pois a funcionalidade básica já é provida pela classe abstrata.
-}
