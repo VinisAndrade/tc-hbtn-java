c8a151c8e60be98867b5b6e7a881404532ffc73f
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-03 17:47:24 -0300

    a

--- Generics_Exercises/armazem/Program.java
+++ Generics_Exercises/armazem/Program.java
@@ -4,7 +4,7 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("pt", "BR"));
         Comida comida1 = new Comida("arroz", 100, 15.9);
-        Comida comida2 = new Comida("feijao", 80, 23.0);
+        Comida comida2 = new Comida("feijao", 80.00, 23.0);
         Comida comida3 = new Comida("pacoca", 165, 7.5);
 
         Eletronico eletronico1 = new Eletronico("Smart TV LED 32 HD LG", 1452.55);
--- Generics_Exercises/armazem_especifico/Eletronico.java
+++ Generics_Exercises/armazem_especifico/Eletronico.java
@@ -17,6 +17,6 @@ public class Eletronico {
 
     @Override
     public String toString() {
-        return String.format("[%s] R$ %.2f", descricao, valor);
+        return String.format("[%s] R$ %.6f", descricao, valor);
     }
 }
