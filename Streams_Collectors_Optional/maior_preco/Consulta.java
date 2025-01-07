4227671ea8a808bb0dc4b767bff01d18908461e5
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-07 16:12:25 -0300

    Update Consulta.java

--- Streams_Collectors_Optional/maior_preco/Consulta.java
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -1,18 +1,18 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Consulta {
-
     public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
-        return pedido.getProdutos().stream()
-                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
-                .collect(Collectors.toList());
+        return pedido.getProdutos()
+                     .stream()
+                     .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+                     .collect(Collectors.toList());
     }
 
-    public static Optional<Produto> obterProdutoMaiorPreco(List<Produto> produtos) {
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
         return produtos.stream()
-                .max(Comparator.comparing(Produto::getPreco));
+                       .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                       .collect(Collectors.toList())
+                       .get(0);
     }
 }
