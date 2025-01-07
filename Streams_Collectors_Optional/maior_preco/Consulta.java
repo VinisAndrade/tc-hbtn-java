2e62583104c4a8ec29b0fd74eab40d21c04d7637
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-07 16:06:21 -0300

    task1

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,18 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Optional<Produto> obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .max(Comparator.comparing(Produto::getPreco));
+    }
+}
