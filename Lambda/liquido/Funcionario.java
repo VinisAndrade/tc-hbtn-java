a130bb16a7c42ccc7a8f91ee9d3385b9e3aa8f5d
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-07 10:33:58 -0300

    1. Salario Funcionario

---
+++ Lambda/liquido/Funcionario.java
@@ -0,0 +1,18 @@
+public class Funcionario {
+    private double salario;
+
+    // Construtor
+    public Funcionario(double salario) {
+        this.salario = salario;
+    }
+
+    // Getter
+    public double getSalario() {
+        return salario;
+    }
+
+    // Setter (opcional, caso necessário alterar o salário)
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
