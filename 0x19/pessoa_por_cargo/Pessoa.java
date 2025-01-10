fb2bf22c237137ae1e2531f585636bdc46d69f9e
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-10 11:23:34 -0300

    a

--- 0x19/pessoa_por_cargo/Pessoa.java
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -1,13 +1,12 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
-class Pessoa {
+public class Pessoa {
     private int codigo;
     private String nome;
     private String cargo;
     private int idade;
     private double salario;
-
+    
     public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
         this.codigo = codigo;
         this.nome = nome;
@@ -16,12 +15,30 @@ class Pessoa {
         this.salario = salario;
     }
 
-    @Override
-    public String toString() {
-        return "[" + codigo + "] " + nome + " " + cargo + " " + idade + " R$ " + String.format("%.2f", salario);
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
     }
 
     public String getCargo() {
         return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+    	Locale.setDefault(new Locale("pt", "BR"));
+        return String.format("[%d] %s %s %d R$ %f", getCodigo(),
+            getNome(), getCargo(), getIdade(), getSalario());
     }
 }
