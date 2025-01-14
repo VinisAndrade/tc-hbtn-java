85e0a01d99484269f4ba9f13609aef2219551387
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-14 17:52:10 -0300

    0. Pessoas agrupadas por cargos em order reversa

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -1,14 +1,12 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
-// Classe Pessoa
-class Pessoa implements Comparable<Pessoa> {
+public class Pessoa implements Comparable {
     private int codigo;
     private String nome;
     private String cargo;
     private int idade;
     private double salario;
-
+    
     public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
         this.codigo = codigo;
         this.nome = nome;
@@ -17,17 +15,36 @@ class Pessoa implements Comparable<Pessoa> {
         this.salario = salario;
     }
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
     public String getCargo() {
         return cargo;
     }
 
-    @Override
-    public String toString() {
-        return String.format("[%d] %s %s %d R$ %.2f", codigo, nome, cargo, idade, salario);
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
     }
 
     @Override
-    public int compareTo(Pessoa outra) {
-        return Integer.compare(this.codigo, outra.codigo);
+    public String toString() {
+    	Locale.setDefault(new Locale("pt", "BR"));
+        return String.format("[%d] %s %s %d R$ %f", getCodigo(),
+            getNome(), getCargo(), getIdade(), getSalario());
+    }
+
+    @Override
+    public int compareTo(Object obj) {
+        Pessoa pessoa = (Pessoa) obj;
+        return this.getNome().compareTo(pessoa.getNome());
     }
 }
