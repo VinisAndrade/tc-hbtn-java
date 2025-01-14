b6bfffc3f3185e97c90a7d498aa83c5fabd6a444
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-14 17:58:22 -0300

    1. Contagem de pessoas por cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/Pessoa.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+// Classe Pessoa
+class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.2f", codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        return Integer.compare(this.codigo, outra.codigo);
+    }
+}
