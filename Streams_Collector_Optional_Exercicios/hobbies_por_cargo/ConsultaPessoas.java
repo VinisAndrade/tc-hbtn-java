c7bcf60f0b9662c4be86431d03aaa1504f7f9357
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-14 18:20:28 -0300

    4. Hobbies por cargo

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class ConsultaPessoas {
+    public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                Collectors.flatMapping(
+                    pessoa -> pessoa.getHobbies().stream(),
+                    Collectors.toCollection(TreeSet::new)
+                )
+            ));
+    }
+}
---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/Pessoa.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+    private List<String> hobbies;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+        this.hobbies = hobbies;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.2f", codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public int compareTo(Pessoa outra) {
+        return Integer.compare(this.codigo, outra.codigo);
+    }
+}
