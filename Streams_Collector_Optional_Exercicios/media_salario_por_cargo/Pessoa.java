f2cb6405b843140f23f63fb0a917dc2a6399941e
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-14 18:16:32 -0300

    Update Pessoa.java

--- Streams_Collector_Optional_Exercicios/media_salario_por_cargo/Pessoa.java
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/Pessoa.java
@@ -20,6 +20,10 @@ class Pessoa implements Comparable<Pessoa> {
         return cargo;
     }
 
+    public int getIdade() {
+        return idade;
+    }
+
     public double getSalario() {
         return salario;
     }
